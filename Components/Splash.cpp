0a391ef705d7e5ca8bc638b8b8a78138b6107173
to1dev <admin@to1.dev>
2025-02-02 02:05:05 +0100

    update SplashThread and Splash

--- Components/Splash.cpp
+++ Components/Splash.cpp
@@ -2,89 +2,255 @@
 
 namespace Daitengu::Components {
 
-#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
-
-HBITMAP QImageToHBITMAP(const QImage& image)
+SplashThread::SplashThread(QObject* parent)
+    : QThread(parent)
 {
-    QImage convertedImage = image.convertToFormat(QImage::Format_RGBA8888);
+}
 
-    BITMAPINFO bmi = {};
-    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmi.bmiHeader.biWidth = convertedImage.width();
-    bmi.bmiHeader.biHeight = -convertedImage.height();
-    bmi.bmiHeader.biPlanes = 1;
-    bmi.bmiHeader.biBitCount = 32;
-    bmi.bmiHeader.biCompression = BI_RGB;
+void SplashThread::sleep(unsigned long secs)
+{
+    QThread::sleep(secs);
+}
 
-    HDC hdc = GetDC(NULL);
-    HBITMAP hBitmap = CreateDIBitmap(hdc, &bmi.bmiHeader, CBM_INIT,
-        convertedImage.constBits(), &bmi, DIB_RGB_COLORS);
-    ReleaseDC(NULL, hdc);
+void SplashThread::msleep(unsigned long msecs)
+{
+    QThread::msleep(msecs);
+}
 
-    return hBitmap;
+void SplashThread::usleep(unsigned long usecs)
+{
+    QThread::usleep(usecs);
+}
+
+bool SplashThread::interruptibleSleep(unsigned long secs)
+{
+    const unsigned long checkInterval = 100;
+    unsigned long remainingTime = secs * 1000;
+
+    while (remainingTime > 0 && !isInterruptionRequested()) {
+        unsigned long sleepTime = std::min(remainingTime, checkInterval);
+        msleep(sleepTime);
+        remainingTime -= sleepTime;
+    }
+
+    return !isInterruptionRequested();
+}
+
+void SplashThread::run()
+{
+    emit started();
+    exec();
+    emit finished();
+}
+
+void SplashThread::requestStop()
+{
+    requestInterruption();
+    quit();
+    wait();
+}
+
+SplashThreadEx::SplashThreadEx(QObject* parent)
+    : SplashThread(parent)
+    , isPaused_(false)
+{
+}
+
+void SplashThreadEx::pause()
+{
+    mutex_.lock();
+    isPaused_ = true;
+    mutex_.unlock();
+}
+
+void SplashThreadEx::resume()
+{
+    mutex_.lock();
+    isPaused_ = false;
+    condition_.wakeAll();
+    mutex_.unlock();
+}
+
+bool SplashThreadEx::interruptibleSleepWithTimeout(
+    unsigned long secs, unsigned long timeoutSecs)
+{
+    QDeadlineTimer deadline(timeoutSecs * 1000);
+
+    const unsigned long checkInterval = 100;
+    unsigned long remainingTime = secs * 1000;
+
+    while (remainingTime > 0 && !isInterruptionRequested()) {
+        if (deadline.hasExpired()) {
+            emit error("Operation timed out");
+            return false;
+        }
+
+        checkPause();
+
+        unsigned long sleepTime = std::min(remainingTime, checkInterval);
+        msleep(sleepTime);
+        remainingTime -= sleepTime;
+    }
+
+    return !isInterruptionRequested();
+}
+
+void SplashThreadEx::checkPause()
+{
+    mutex_.lock();
+    while (isPaused_ && !isInterruptionRequested()) {
+        condition_.wait(&mutex_);
+    }
+    mutex_.unlock();
+}
+
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+HBITMAP QImageToHBITMAP(const QImage& image, bool premultiplied = false)
+{
+    try {
+        QImage convertedImage = premultiplied
+            ? image.convertToFormat(QImage::Format_ARGB32_Premultiplied)
+            : image.convertToFormat(QImage::Format_RGBA8888);
+
+        BITMAPINFO bmi = {};
+        bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bmi.bmiHeader.biWidth = convertedImage.width();
+        bmi.bmiHeader.biHeight = -convertedImage.height();
+        bmi.bmiHeader.biPlanes = 1;
+        bmi.bmiHeader.biBitCount = 32;
+        bmi.bmiHeader.biCompression = BI_RGB;
+
+        DCWrapper hdc;
+        HBITMAP hBitmap = CreateDIBitmap(hdc, &bmi.bmiHeader, CBM_INIT,
+            convertedImage.constBits(), &bmi, DIB_RGB_COLORS);
+
+        if (!hBitmap) {
+            LOG_ERROR("Failed to create bitmap");
+            throw std::runtime_error("Failed to create bitmap");
+        }
+
+        return hBitmap;
+    } catch (const std::exception& e) {
+        LOG_ERROR(QString("QImageToHBITMAP failed: %1").arg(e.what()));
+        throw;
+    }
 }
 
 HBITMAP QImageToHBITMAPWithPremultipliedAlpha(const QImage& image)
 {
-    QImage premultipliedImage
-        = image.convertToFormat(QImage::Format_ARGB32_Premultiplied);
-
-    BITMAPINFO bmi = {};
-    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmi.bmiHeader.biWidth = premultipliedImage.width();
-    bmi.bmiHeader.biHeight = -premultipliedImage.height();
-    bmi.bmiHeader.biPlanes = 1;
-    bmi.bmiHeader.biBitCount = 32;
-    bmi.bmiHeader.biCompression = BI_RGB;
-
-    HDC hdc = GetDC(NULL);
-    HBITMAP hBitmap = CreateDIBitmap(hdc, &bmi.bmiHeader, CBM_INIT,
-        premultipliedImage.constBits(), &bmi, DIB_RGB_COLORS);
-    ReleaseDC(NULL, hdc);
-
-    return hBitmap;
+    return QImageToHBITMAP(image, true);
 }
 #endif
 
-Splash::Splash(const QPixmap& pixmap)
+Splash::Splash(const QPixmap& pixmap, const SplashConfig& config)
     : QSplashScreen(pixmap)
+    , config_(config)
 {
-    HDC dc;
-    RECT rc;
-    POINT pt;
-    POINT pt2;
-    SIZE sz;
-    HWND handle = (HWND)winId();
-    BLENDFUNCTION bf = { AC_SRC_OVER, 0, 0xFF, AC_SRC_ALPHA };
-    SetWindowLong(handle, GWL_EXSTYLE,
-        GetWindowLong(handle, GWL_EXSTYLE) | WS_EX_LAYERED);
-    SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-
-    dc = GetDC(0);
-    GetWindowRect(handle, &rc);
-    pt.x = rc.left;
-    pt.y = rc.top;
-    pt2.x = 0;
-    pt2.y = 0;
-    sz.cx = pixmap.width();
-    sz.cy = pixmap.height();
-
-    HDC dc2 = CreateCompatibleDC(dc);
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    SelectObject(
-        dc2, QtWin::toHBITMAP(pixmap, QtWin::HBitmapPremultipliedAlpha));
-#else
-    SelectObject(dc2, QImageToHBITMAPWithPremultipliedAlpha(pixmap.toImage()));
-#endif
-    UpdateLayeredWindow(handle, dc, &pt, &sz, dc2, &pt2, 0, &bf, ULW_ALPHA);
-    ReleaseDC(0, dc);
-    ReleaseDC(0, dc2);
+    LOG_INFO("Initializing Splash Screen");
+    try {
+        initializeWindow(pixmap);
+    } catch (const std::exception& e) {
+        LOG_ERROR(
+            QString("Failed to initialize splash screen: %1").arg(e.what()));
+        recover();
+    }
 }
 
 void Splash::stayOnTop()
 {
-    SetWindowPos(
-        (HWND)winId(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+    try {
+        if (auto handle = reinterpret_cast<HWND>(winId())) {
+            if (!SetWindowPos(handle, HWND_TOPMOST, 0, 0, 0, 0,
+                    SWP_NOMOVE | SWP_NOSIZE)) {
+                LOG_ERROR("Failed to set window position");
+            }
+        }
+    } catch (const std::exception& e) {
+        LOG_ERROR(QString("stayOnTop failed: %1").arg(e.what()));
+    }
+}
+
+void Splash::setOpacity(BYTE opacity)
+{
+    config_.opacity = opacity;
+    updateLayeredWindow();
+}
+
+void Splash::initializeWindow(const QPixmap& pixmap)
+{
+    auto handle = reinterpret_cast<HWND>(winId());
+    if (!handle) {
+        LOG_ERROR("Invalid window handle");
+        return;
+    }
+
+    auto style = GetWindowLong(handle, GWL_EXSTYLE);
+    if (!SetWindowLong(handle, GWL_EXSTYLE, style | WS_EX_LAYERED)) {
+        LOG_ERROR("Failed to set window style");
+        return;
+    }
+
+    if (config_.alwaysOnTop) {
+        stayOnTop();
+    }
+
+    updateLayeredWindow(pixmap);
+}
+
+void Splash::updateLayeredWindow(const QPixmap& pixmap)
+{
+    auto handle = reinterpret_cast<HWND>(winId());
+    if (!handle)
+        return;
+
+    BLENDFUNCTION bf = { AC_SRC_OVER, 0, config_.opacity, AC_SRC_ALPHA };
+    RECT rc;
+    GetWindowRect(handle, &rc);
+
+    POINT ptDst = { rc.left, rc.top };
+    POINT ptSrc = { 0, 0 };
+    SIZE size = { width(), height() };
+
+    try {
+        DCWrapper dcScreen;
+        std::unique_ptr<HDC__, decltype(&DeleteDC)> dcMem(
+            CreateCompatibleDC(dcScreen), DeleteDC);
+
+        if (!dcMem) {
+            LOG_ERROR("Failed to create compatible DC");
+            return;
+        }
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+        auto hBitmap
+            = QtWin::toHBITMAP(pixmap.isNull() ? this->pixmap() : pixmap,
+                QtWin::HBitmapPremultipliedAlpha);
+#else
+        auto hBitmap = QImageToHBITMAPWithPremultipliedAlpha(
+            (pixmap.isNull() ? this->pixmap() : pixmap).toImage());
+#endif
+
+        BitmapSelector selector(dcMem.get(), hBitmap);
+
+        if (!UpdateLayeredWindow(handle, dcScreen, &ptDst, &size, dcMem.get(),
+                &ptSrc, 0, &bf, config_.layeredFlags)) {
+            LOG_ERROR("UpdateLayeredWindow failed");
+        }
+
+        DeleteObject(hBitmap);
+    } catch (const std::exception& e) {
+        LOG_ERROR(QString("updateLayeredWindow failed: %1").arg(e.what()));
+    }
+}
+
+void Splash::recover()
+{
+    LOG_INFO("Attempting to recover from error...");
+    if (auto handle = reinterpret_cast<HWND>(winId())) {
+        SetWindowLong(handle, GWL_EXSTYLE, WS_EX_LAYERED);
+        ShowWindow(handle, SW_SHOW);
+        UpdateWindow(handle);
+    }
 }
 
 }
--- Components/Splash.h
+++ Components/Splash.h
@@ -1,6 +1,10 @@
 #ifndef SPLASH_H
 #define SPLASH_H
 
+#include <memory>
+#include <stdexcept>
+
+#include <QDebug>
 #include <QSplashScreen>
 #include <QThread>
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
@@ -13,19 +17,125 @@ namespace Daitengu::Components {
 
 inline constexpr int SLEEP_TIME = 1;
 
+#define LOG_ERROR(msg) qCritical() << "[Splash Error]" << msg
+#define LOG_INFO(msg) qInfo() << "[Splash Info]" << msg
+
 class SplashThread : public QThread {
+    Q_OBJECT
+
 public:
-    static void sleep(unsigned long secs)
+    explicit SplashThread(QObject* parent = nullptr);
+
+    static void sleep(unsigned long secs);
+    static void msleep(unsigned long msecs);
+    static void usleep(unsigned long usecs);
+
+    bool interruptibleSleep(unsigned long secs);
+
+protected:
+    void run() override;
+
+signals:
+    void started();
+    void finished();
+    void error(const QString& errorMessage);
+
+public slots:
+    void requestStop();
+};
+
+class SplashThreadEx : public SplashThread {
+    Q_OBJECT
+
+public:
+    explicit SplashThreadEx(QObject* parent = nullptr);
+
+    void pause();
+    void resume();
+
+    bool interruptibleSleepWithTimeout(
+        unsigned long secs, unsigned long timeoutSecs);
+
+protected:
+    void checkPause();
+
+private:
+    QMutex mutex_;
+    QWaitCondition condition_;
+    bool isPaused_;
+};
+
+struct SplashConfig {
+    bool alwaysOnTop = true;
+    BYTE opacity = 0xFF;
+    DWORD layeredFlags = ULW_ALPHA;
+};
+
+class DCWrapper {
+public:
+    DCWrapper(HWND h = NULL)
+        : hwnd(h)
     {
-        QThread::sleep(secs);
+        dc = GetDC(hwnd);
+        if (!dc) {
+            LOG_ERROR("Failed to get DC");
+            throw std::runtime_error("Failed to get DC");
+        }
     }
+
+    ~DCWrapper()
+    {
+        if (dc)
+            ReleaseDC(hwnd, dc);
+    }
+
+    operator HDC() const
+    {
+        return dc;
+    }
+
+private:
+    HDC dc;
+    HWND hwnd;
+};
+
+class BitmapSelector {
+public:
+    BitmapSelector(HDC hdc, HBITMAP bitmap)
+        : dc(hdc)
+    {
+        if (!bitmap) {
+            LOG_ERROR("Invalid bitmap handle");
+            throw std::runtime_error("Invalid bitmap handle");
+        }
+        oldBitmap = (HBITMAP)SelectObject(dc, bitmap);
+    }
+
+    ~BitmapSelector()
+    {
+        if (oldBitmap)
+            SelectObject(dc, oldBitmap);
+    }
+
+private:
+    HDC dc;
+    HBITMAP oldBitmap;
 };
 
 class Splash : public QSplashScreen {
 public:
-    Splash(const QPixmap& pixmap = QPixmap());
+    explicit Splash(
+        const QPixmap& pixmap, const SplashConfig& config = SplashConfig());
 
     void stayOnTop();
+    void setOpacity(BYTE opacity);
+
+private:
+    SplashConfig config_;
+
+    void initializeWindow(const QPixmap& pixmap);
+    void updateLayeredWindow(const QPixmap& pixmap = QPixmap());
+    void recover();
 };
 
 }

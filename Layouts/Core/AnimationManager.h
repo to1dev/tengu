22277cd10664e3760eb7a43c82911dea5419aee3
to1dev <admin@to1.dev>
2025-02-18 21:58:02 +0100

    add AnimationManager class

--- Layouts/Core/AnimationManager.h
+++ Layouts/Core/AnimationManager.h
@@ -5,6 +5,7 @@
 #include <QObject>
 #include <QParallelAnimationGroup>
 #include <QPropertyAnimation>
+#include <QWidget>
 
 #include "Types.h"
 
@@ -17,8 +18,31 @@ public:
     explicit AnimationManager(QObject* parent = nullptr);
     ~AnimationManager();
 
+    void startAnimation(QWidget* window, const QRect& targetGeometry,
+        const AnimationConfig& config);
+    void startGroupAnimation(
+        const QMap<QWidget*, QRect>& animations, const AnimationConfig& config);
+    void stopAll();
+    bool isRunning() const;
+
+    void setDefaultConfig(const AnimationConfig& config);
+    AnimationConfig defaultConfig() const;
+
 Q_SIGNALS:
     void animationStarted(QWidget* window);
+    void animationFinished(QWidget* window);
+    void groupAnimationStarted();
+    void groupAnimationFinished();
+
+private:
+    QMap<QWidget*, QPropertyAnimation*> activeAnimations_;
+    QList<QParallelAnimationGroup*> activeGroups_;
+    AnimationConfig defaultConfig_;
+
+    void cleanupAnimation(QWidget* window);
+    void cleanupGroup(QParallelAnimationGroup* group);
+    QPropertyAnimation* createAnimation(QWidget* window,
+        const QRect& targetGeometry, const AnimationConfig& config);
 };
 
 }

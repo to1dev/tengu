1fc6e2975cee3d758fda023bb92ef0c4a4d71dd4
to1dev <admin@to1.dev>
2025-02-20 17:25:19 +0100

    add hideAddress functions

--- Utils/Helpers.hpp
+++ Utils/Helpers.hpp
@@ -1,7 +1,16 @@
 #ifndef HELPERS_HPP
 #define HELPERS_HPP
 
+#include <cstdint>
+#include <cstring>
+#include <iostream>
 #include <random>
+#include <regex>
+#include <string>
+
+#include <QLocale>
+#include <QRegularExpression>
+#include <QString>
 
 namespace Daitengu::Utils {
 
@@ -11,6 +20,25 @@ inline int randomIndex(int start, int end)
     std::mt19937 generator(device());
     std::uniform_int_distribution<int> distribution(start, end);
     return distribution(generator);
+}
+
+inline std::string hideAddress(std::string_view address)
+{
+    if (address.length() <= 10) {
+        return std::string(address);
+    }
+
+    return std::string(address.substr(0, 5)) + "..."
+        + std::string(address.substr(address.size() - 5, 5));
+}
+
+inline QString hideAddress(const QString& address)
+{
+    if (address.length() <= 10) {
+        return address;
+    }
+
+    return address.left(5) + "..." + address.right(5);
 }
 
 }

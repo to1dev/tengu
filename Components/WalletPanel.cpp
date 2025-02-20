9081966b3245845adef701cd99cc47cb17079440
to1dev <admin@to1.dev>
2025-02-20 19:14:58 +0100

    update WalletPanel

--- Components/WalletPanel.cpp
+++ Components/WalletPanel.cpp
@@ -2,6 +2,96 @@
 
 namespace Daitengu::Components {
 
+UserCard::UserCard(QWidget* parent)
+    : QWidget(parent)
+{
+    setObjectName("userCard");
+
+    auto* mainLayout = new QVBoxLayout(this);
+    auto* topLayout = new QHBoxLayout();
+    auto* bottomLayout = new QHBoxLayout();
+
+    int index = randomIndex(0, RandomAvatars.size() - 1);
+    int range = randomIndex(
+        RandomAvatars[index].second.start, RandomAvatars[index].second.end);
+
+    SVGWidget* svgAvatar = new SVGWidget(
+        QString(":/%1/%2").arg(RandomAvatars[index].first).arg(range), this, 3);
+    svgAvatar->setFixedSize(AVATAR_SIZE, AVATAR_SIZE);
+
+    topLayout->setSpacing(6);
+    topLayout->addWidget(svgAvatar);
+
+    QVBoxLayout* textLayout = new QVBoxLayout();
+
+    nameLabel_ = new QLabel(this);
+    nameLabel_->setText("Satoshi Nakamoto");
+    nameLabel_->setObjectName(STR_NAME_LABEL);
+
+    QHBoxLayout* addressLayout = new QHBoxLayout();
+
+    addressLabel_ = new ClickableLabel(this);
+    addressLabel_->setObjectName(STR_ADDRESS_LABEL);
+    QString address("So11111111111111111111111111111111111111112");
+    addressLabel_->setCursor(Qt::PointingHandCursor);
+    addressLabel_->setText(hideAddress(address));
+
+    addressLayout->addWidget(addressLabel_);
+    addressLayout->addStretch(1);
+
+    textLayout->setSpacing(6);
+    textLayout->addWidget(nameLabel_);
+    textLayout->addLayout(addressLayout);
+
+    topLayout->addLayout(textLayout);
+
+    QPushButton* selectButton = new QPushButton(STR_SELECT_BUTTON_TEXT, this);
+    selectButton->setObjectName(STR_SELECT_BUTTON);
+    selectButton->setMinimumHeight(30);
+    selectButton->setMaximumHeight(30);
+
+    bottomLayout->setSpacing(6);
+    bottomLayout->addWidget(selectButton);
+
+    mainLayout->setSpacing(20);
+    mainLayout->addLayout(topLayout);
+    mainLayout->addLayout(bottomLayout);
+}
+
+ValueCard::ValueCard(QWidget* parent)
+    : QWidget(parent)
+{
+    setObjectName("valueCard");
+    auto* mainLayout = new QVBoxLayout(this);
+    auto* valueLayout = new QVBoxLayout();
+
+    valueLabel_ = new QLabel(this);
+    valueLabel_->setObjectName(STR_VALUE_LABEL);
+    valueLabel_->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+
+    valueLabel_->setText("1.20 SOL");
+
+    valueLayout->addWidget(valueLabel_);
+    mainLayout->addLayout(valueLayout);
+}
+
+std::uint64_t ValueCard::value() const
+{
+    return value_;
+}
+
+void ValueCard::setValue(std::uint64_t newValue)
+{
+    value_ = newValue;
+}
+
+ObjectsCard::ObjectsCard(QWidget* parent)
+    : AnimatedTabWidget(parent)
+{
+    setObjectName("objectsCard");
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+}
+
 WalletPanel::WalletPanel(QWidget* parent)
     : QFrame(parent)
 {
@@ -24,10 +114,29 @@ WalletPanel::WalletPanel(QWidget* parent)
     frameValue->setObjectName("walletFrame");
 
     QVBoxLayout* layoutValue = new QVBoxLayout(frameValue);
+    valueCard_ = new ValueCard(frameValue);
+    layoutValue->addWidget(valueCard_);
     frameValue->setLayout(layoutValue);
 
+    QWidget* frameObjects = new QWidget(this);
+    frameObjects->setObjectName("objectsFrame");
+
+    QVBoxLayout* layoutObjects = new QVBoxLayout(frameObjects);
+    layoutObjects->setSpacing(9);
+
+    LineEditEx* editFilter = new LineEditEx(frameObjects);
+    editFilter->setObjectName("editFilter");
+    editFilter->setMaxLength(64);
+
+    objectsCard_ = new ObjectsCard(this);
+
+    layoutObjects->addWidget(editFilter);
+    layoutObjects->addWidget(objectsCard_, 1);
+    frameObjects->setLayout(layoutObjects);
+
     layout->addWidget(frameUser);
-    layout->addWidget(frameValue, 1);
+    layout->addWidget(frameValue);
+    layout->addWidget(frameObjects, 1);
     setLayout(layout);
 }
 
@@ -36,47 +145,14 @@ UserCard* WalletPanel::userCard() const
     return userCard_;
 }
 
-UserCard::UserCard(QWidget* parent)
-    : QWidget(parent)
+ValueCard* WalletPanel::valueCard() const
 {
-    setObjectName("userCard");
-    auto* mainLayout = new QVBoxLayout(this);
-    auto* topLayout = new QHBoxLayout();
-    auto* bottomLayout = new QHBoxLayout();
+    return valueCard_;
+}
 
-    int index = randomIndex(0, RandomAvatars.size() - 1);
-    int range = randomIndex(
-        RandomAvatars[index].second.start, RandomAvatars[index].second.end);
-    SVGWidget* svgAvatar = new SVGWidget(
-        QString(":/%1/%2").arg(RandomAvatars[index].first).arg(range), this, 3);
-    svgAvatar->setFixedSize(AVATAR_SIZE, AVATAR_SIZE);
-    topLayout->setSpacing(6);
-    topLayout->addWidget(svgAvatar);
-
-    QVBoxLayout* textLayout = new QVBoxLayout();
-    nameLabel_ = new QLabel(this);
-    nameLabel_->setText("冲天小羊羔吖");
-    nameLabel_->setObjectName(STR_NAME_LABEL);
-    addressLabel_ = new ClickableLabel(this);
-    addressLabel_->setObjectName(STR_ADDRESS_LABEL);
-    QString address("So11111111111111111111111111111111111111112");
-    addressLabel_->setCursor(Qt::PointingHandCursor);
-    addressLabel_->setText(address);
-    textLayout->setSpacing(6);
-    textLayout->addWidget(nameLabel_);
-    textLayout->addWidget(addressLabel_);
-    topLayout->addLayout(textLayout);
-
-    QPushButton* selectButton = new QPushButton(STR_SELECT_BUTTON_TEXT, this);
-    selectButton->setObjectName(STR_SELECT_BUTTON);
-    selectButton->setMinimumHeight(30);
-    selectButton->setMaximumHeight(30);
-    bottomLayout->setSpacing(6);
-    bottomLayout->addWidget(selectButton);
-
-    mainLayout->setSpacing(20);
-    mainLayout->addLayout(topLayout);
-    mainLayout->addLayout(bottomLayout);
+ObjectsCard* WalletPanel::objectsCard() const
+{
+    return objectsCard_;
 }
 
 }
--- Components/WalletPanel.h
+++ Components/WalletPanel.h
@@ -12,7 +12,9 @@
 
 #include "Utils/Helpers.hpp"
 
+#include "AnimatedTabWidget.h"
 #include "ClickableLabel.h"
+#include "LineEditEx.h"
 #include "SVGWidget.h"
 
 using namespace Daitengu::Core;
@@ -23,6 +25,7 @@ namespace Daitengu::Components {
 inline constexpr char STR_NAME_LABEL[] = "nameLabel";
 inline constexpr char STR_ADDRESS_LABEL[] = "addressLabel";
 inline constexpr char STR_SELECT_BUTTON[] = "selectButton";
+inline constexpr char STR_VALUE_LABEL[] = "valueLabel";
 
 inline const QString STR_SELECT_BUTTON_TEXT = QObject::tr("切换");
 
@@ -37,6 +40,27 @@ private:
     ClickableLabel* addressLabel_;
 };
 
+class ValueCard : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit ValueCard(QWidget* parent = nullptr);
+
+    std::uint64_t value() const;
+    void setValue(std::uint64_t newValue);
+
+private:
+    QLabel* valueLabel_;
+    std::uint64_t value_ { 0 };
+};
+
+class ObjectsCard : public AnimatedTabWidget {
+    Q_OBJECT
+
+public:
+    explicit ObjectsCard(QWidget* parent = nullptr);
+};
+
 class WalletPanel : public QFrame {
     Q_OBJECT
 
@@ -47,8 +71,14 @@ public:
 
     UserCard* userCard() const;
 
+    ValueCard* valueCard() const;
+
+    ObjectsCard* objectsCard() const;
+
 private:
     UserCard* userCard_;
+    ValueCard* valueCard_;
+    ObjectsCard* objectsCard_;
 };
 
 }

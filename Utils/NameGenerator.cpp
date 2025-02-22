b1dfc9ffa7fc9970aadd9f6a16c19d497c01c4e2
to1dev <admin@to1.dev>
2025-02-22 19:19:42 +0100

    add NameGenerator

---
+++ Utils/NameGenerator.cpp
@@ -0,0 +1,486 @@
+#include "NameGenerator.h"
+
+namespace Daitengu::Utils {
+
+static std::mt19937 rng(
+    std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+std::wstring towstring(const std::string& s)
+{
+    if (s.empty()) {
+        return std::wstring();
+    }
+
+    std::mbstate_t state {};
+    const char* src = s.c_str();
+    std::size_t len = std::mbsrtowcs(nullptr, &src, 0, &state);
+    if (len == static_cast<std::size_t>(-1)) {
+        return std::wstring();
+    }
+
+    std::vector<wchar_t> buf(len + 1, L'\0');
+    src = s.c_str();
+    std::mbsrtowcs(buf.data(), &src, len, &state);
+    return std::wstring(buf.data());
+}
+
+std::string tostring(const std::wstring& ws)
+{
+    if (ws.empty()) {
+        return std::string();
+    }
+
+    std::mbstate_t state {};
+    const wchar_t* src = ws.c_str();
+    std::size_t len = std::wcsrtombs(nullptr, &src, 0, &state);
+    if (len == static_cast<std::size_t>(-1)) {
+        return std::string();
+    }
+
+    std::vector<char> buf(len + 1, '\0');
+    src = ws.c_str();
+    std::wcsrtombs(buf.data(), &src, len, &state);
+    return std::string(buf.data());
+}
+
+NameGenerator::NameGenerator() = default;
+
+NameGenerator::NameGenerator(
+    std::vector<std::unique_ptr<NameGenerator>>&& generators_)
+    : generators(std::move(generators_))
+{
+}
+
+NameGenerator::NameGenerator(const std::string& pattern, bool collapse_triples)
+{
+    std::unique_ptr<NameGenerator> last;
+
+    // We parse the pattern using Group stacks
+    std::stack<std::unique_ptr<Group>> parseStack;
+    std::unique_ptr<Group> top
+        = std::make_unique<GroupSymbol>(); // initial: symbol group
+
+    for (char c : pattern) {
+        switch (c) {
+        case '<':
+            parseStack.push(std::move(top));
+            top = std::make_unique<GroupSymbol>();
+            break;
+        case '(':
+            parseStack.push(std::move(top));
+            top = std::make_unique<GroupLiteral>();
+            break;
+        case '>':
+        case ')':
+            if (parseStack.empty()) {
+                throw std::invalid_argument(
+                    "Unbalanced brackets: extra '>' or ')'");
+            }
+            if (c == '>' && top->type != group_types::symbol) {
+                throw std::invalid_argument(
+                    "Unexpected '>' outside symbol group");
+            }
+            if (c == ')' && top->type != group_types::literal) {
+                throw std::invalid_argument(
+                    "Unexpected ')' outside literal group");
+            }
+            last = top->produce();
+            top = std::move(parseStack.top());
+            parseStack.pop();
+            top->add(std::move(last));
+            break;
+        case '|':
+            top->split();
+            break;
+        case '!':
+            if (top->type == group_types::symbol) {
+                top->wrap(wrappers::capitalizer);
+            } else {
+                // If inside literal, treat '!' as normal char
+                top->add(c);
+            }
+            break;
+        case '~':
+            if (top->type == group_types::symbol) {
+                top->wrap(wrappers::reverser);
+            } else {
+                top->add(c);
+            }
+            break;
+        default:
+            top->add(c);
+            break;
+        }
+    }
+
+    if (!parseStack.empty()) {
+        throw std::invalid_argument("Unbalanced brackets: missing '>' or ')'");
+    }
+
+    std::unique_ptr<NameGenerator> g = top->produce();
+    if (collapse_triples) {
+        g = std::make_unique<Collapser>(std::move(g));
+    }
+    add(std::move(g));
+}
+
+std::size_t NameGenerator::combinations()
+{
+    std::size_t total = 1;
+    for (auto& g : generators) {
+        total *= g->combinations();
+    }
+    return total;
+}
+
+std::size_t NameGenerator::min()
+{
+    std::size_t total = 0;
+    for (auto& g : generators) {
+        total += g->min();
+    }
+    return total;
+}
+
+std::size_t NameGenerator::max()
+{
+    std::size_t total = 0;
+    for (auto& g : generators) {
+        total += g->max();
+    }
+    return total;
+}
+
+std::string NameGenerator::toString()
+{
+    // In the base case, we just concatenate the results of child generators
+    std::string result;
+    for (auto& g : generators) {
+        result += g->toString();
+    }
+    return result;
+}
+
+void NameGenerator::add(std::unique_ptr<NameGenerator>&& g)
+{
+    generators.push_back(std::move(g));
+}
+
+NameGenerator::Group::Group(group_types t)
+    : type(t)
+{
+}
+
+std::unique_ptr<NameGenerator> NameGenerator::Group::produce()
+{
+    if (set.empty()) {
+        // No children => return empty literal
+        return std::make_unique<Literal>("");
+    }
+    if (set.size() == 1) {
+        // If only one child, pass it through
+        return std::move(set[0]);
+    }
+    // Otherwise, random choice among them
+    return std::make_unique<Random>(std::move(set));
+}
+
+void NameGenerator::Group::split()
+{
+    // Insert a new sequence so that next items go there
+    set.push_back(std::make_unique<Sequence>());
+}
+
+void NameGenerator::Group::wrap(wrappers w)
+{
+    wrapperStack.push(w);
+}
+
+void NameGenerator::Group::add(std::unique_ptr<NameGenerator>&& g)
+{
+    // If we have any pending wrappers, apply them first
+    while (!wrapperStack.empty()) {
+        switch (wrapperStack.top()) {
+        case wrappers::capitalizer:
+            g = std::make_unique<Capitalizer>(std::move(g));
+            break;
+        case wrappers::reverser:
+            g = std::make_unique<Reverser>(std::move(g));
+            break;
+        }
+        wrapperStack.pop();
+    }
+
+    if (set.empty()) {
+        // Ensure we have at least one Sequence to add into
+        set.push_back(std::make_unique<Sequence>());
+    }
+    // Add the generator to the last sequence
+    set.back()->add(std::move(g));
+}
+
+void NameGenerator::Group::add(char c)
+{
+    // For single char, wrap in a Random + Literal
+    std::string tmp(1, c);
+    auto r = std::make_unique<Random>();
+    r->add(std::make_unique<Literal>(tmp));
+    add(std::move(r));
+}
+
+NameGenerator::GroupSymbol::GroupSymbol()
+    : Group(group_types::symbol)
+{
+}
+
+void NameGenerator::GroupSymbol::add(char c)
+{
+    std::string key(1, c);
+    auto it = SymbolMap().find(key);
+    if (it == SymbolMap().end()) {
+        // Not in symbol map => treat as literal char
+        Group::add(c);
+        return;
+    }
+    // If found, create a random generator containing all mapped expansions
+    auto r = std::make_unique<Random>();
+    for (auto& val : it->second) {
+        r->add(std::make_unique<Literal>(val));
+    }
+    // Now pass r to base add => could apply wrappers, etc.
+    Group::add(std::move(r));
+}
+
+NameGenerator::GroupLiteral::GroupLiteral()
+    : Group(group_types::literal)
+{
+}
+
+const std::unordered_map<std::string, const std::vector<std::string>>&
+NameGenerator::SymbolMap()
+{
+    // Exactly as in the original code, plus a new "X" key for "Big head
+    // monster"
+    static auto* const symbols = new std::unordered_map<std::string,
+        const std::vector<std::string>>(
+        { { "s",
+              { "ach", "ack", "ad", "age", "ald", "ale", "an", "ang", "ar",
+                  "ard", "as", "ash", "at", "ath", "augh", "aw", "ban", "bel",
+                  "bur", "cer", "cha", "che", "dan", "dar", "del", "den", "dra",
+                  "dyn", "ech", "eld", "elm", "em", "en", "end", "eng", "enth",
+                  "er", "ess", "est", "et", "gar", "gha", "hat", "hin", "hon",
+                  "ia", "ight", "ild", "im", "ina", "ine", "ing", "ir", "is",
+                  "iss", "it", "kal", "kel", "kim", "kin", "ler", "lor", "lye",
+                  "mor", "mos", "nal", "ny", "nys", "old", "om", "on", "or",
+                  "orm", "os", "ough", "per", "pol", "qua", "que", "rad", "rak",
+                  "ran", "ray", "ril", "ris", "rod", "roth", "ryn", "sam",
+                  "say", "ser", "shy", "skel", "sul", "tai", "tan", "tas",
+                  "ther", "tia", "tin", "ton", "tor", "tur", "um", "und", "unt",
+                  "urn", "usk", "ust", "ver", "ves", "vor", "war", "wor",
+                  "yer" } },
+            { "v", { "a", "e", "i", "o", "u", "y" } },
+            { "V",
+                { "a", "e", "i", "o", "u", "y", "ae", "ai", "au", "ay", "ea",
+                    "ee", "ei", "eu", "ey", "ia", "ie", "oe", "oi", "oo", "ou",
+                    "ui" } },
+            { "c",
+                { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p",
+                    "q", "r", "s", "t", "v", "w", "x", "y", "z" } },
+            { "B",
+                { "b", "bl", "br", "c", "ch", "chr", "cl", "cr", "d", "dr", "f",
+                    "g", "h", "j", "k", "l", "ll", "m", "n", "p", "ph", "qu",
+                    "r", "rh", "s", "sch", "sh", "sl", "sm", "sn", "st", "str",
+                    "sw", "t", "th", "thr", "tr", "v", "w", "wh", "y", "z",
+                    "zh" } },
+            { "C",
+                { "b", "c", "ch", "ck", "d", "f", "g", "gh", "h", "k", "l",
+                    "ld", "ll", "lt", "m", "n", "nd", "nn", "nt", "p", "ph",
+                    "q", "r", "rd", "rr", "rt", "s", "sh", "ss", "st", "t",
+                    "th", "v", "w", "y", "z" } },
+            { "i",
+                { "air", "ankle", "ball", "beef", "bone", "bum", "bumble",
+                    "bump", "cheese", "clod", "clot", "clown", "corn", "dip",
+                    "dolt", "doof", "dork", "dumb", "face", "finger", "foot",
+                    "fumble", "goof", "grumble", "head", "knock", "knocker",
+                    "knuckle", "loaf", "lump", "lunk", "meat", "muck", "munch",
+                    "nit", "numb", "pin", "puff", "skull", "snark", "sneeze",
+                    "thimble", "twerp", "twit", "wad", "wimp", "wipe" } },
+            { "m",
+                { "baby", "booble", "bunker", "cuddle", "cuddly", "cutie",
+                    "doodle", "foofie", "gooble", "honey", "kissie", "lover",
+                    "lovey", "moofie", "mooglie", "moopie", "moopsie", "nookum",
+                    "poochie", "poof", "poofie", "pookie", "schmoopie",
+                    "schnoogle", "schnookie", "schnookum", "smooch", "smoochie",
+                    "smoosh", "snoogle", "snoogy", "snookie", "snookum",
+                    "snuggy", "sweetie", "woogle", "woogy", "wookie", "wookum",
+                    "wuddle", "wuddly", "wuggy", "wunny" } },
+            { "M",
+                { "boo", "bunch", "bunny", "cake", "cakes", "cute", "darling",
+                    "dumpling", "dumplings", "face", "foof", "goo", "head",
+                    "kin", "kins", "lips", "love", "mush", "pie", "poo", "pooh",
+                    "pook", "pums" } },
+            { "D",
+                { "b", "bl", "br", "cl", "d", "f", "fl", "fr", "g", "gh", "gl",
+                    "gr", "h", "j", "k", "kl", "m", "n", "p", "th", "w" } },
+            { "d",
+                { "elch", "idiot", "ob", "og", "ok", "olph", "olt", "omph",
+                    "ong", "onk", "oo", "oob", "oof", "oog", "ook", "ooz",
+                    "org", "ork", "orm", "oron", "ub", "uck", "ug", "ulf",
+                    "ult", "um", "umb", "ump", "umph", "un", "unb", "ung",
+                    "unk", "unph", "unt", "uzz" } },
+
+            // NEW: Symbol "X" to produce "Big head monster"-like strings
+            { "X",
+                { "Big head monster", "Tiny foot beast", "Giant claw fiend",
+                    "Little bug goblin", "Huge horn demon",
+                    "Dark wing creature", "Floating eye horror",
+                    "Slime king mutant", "Deep sea crawler",
+                    "Steel shell titan" } } });
+    return *symbols;
+}
+
+Random::Random(std::vector<std::unique_ptr<NameGenerator>>&& generators_)
+    : NameGenerator(std::move(generators_))
+{
+}
+
+std::size_t Random::combinations()
+{
+    // Random: sum of children combos
+    std::size_t total = 0;
+    for (auto& g : generators) {
+        total += g->combinations();
+    }
+    return total == 0 ? 1 : total;
+}
+
+std::size_t Random::min()
+{
+    // Return the minimum 'min()' among children
+    if (generators.empty()) {
+        return 0;
+    }
+    std::size_t res = static_cast<std::size_t>(-1);
+    for (auto& g : generators) {
+        std::size_t m = g->min();
+        if (m < res) {
+            res = m;
+        }
+    }
+    return res;
+}
+
+std::size_t Random::max()
+{
+    // Return the maximum 'max()' among children
+    std::size_t res = 0;
+    for (auto& g : generators) {
+        std::size_t m = g->max();
+        if (m > res) {
+            res = m;
+        }
+    }
+    return res;
+}
+
+std::string Random::toString()
+{
+    if (generators.empty()) {
+        return "";
+    }
+    std::uniform_int_distribution<std::size_t> dist(0, generators.size() - 1);
+    return generators[dist(rng)]->toString();
+}
+
+Sequence::Sequence(std::vector<std::unique_ptr<NameGenerator>>&& generators_)
+    : NameGenerator(std::move(generators_))
+{
+}
+
+Literal::Literal(const std::string& value_)
+    : value(value_)
+{
+}
+
+std::size_t Literal::combinations()
+{
+    return 1;
+}
+
+std::size_t Literal::min()
+{
+    return value.size();
+}
+
+std::size_t Literal::max()
+{
+    return value.size();
+}
+
+std::string Literal::toString()
+{
+    return value;
+}
+
+Reverser::Reverser(std::unique_ptr<NameGenerator>&& g)
+{
+    add(std::move(g));
+}
+
+std::string Reverser::toString()
+{
+    std::wstring ws = towstring(NameGenerator::toString());
+    std::reverse(ws.begin(), ws.end());
+    return tostring(ws);
+}
+
+Capitalizer::Capitalizer(std::unique_ptr<NameGenerator>&& g)
+{
+    add(std::move(g));
+}
+
+std::string Capitalizer::toString()
+{
+    std::wstring ws = towstring(NameGenerator::toString());
+    if (!ws.empty()) {
+        ws[0] = std::towupper(ws[0]);
+    }
+    return tostring(ws);
+}
+
+Collapser::Collapser(std::unique_ptr<NameGenerator>&& g)
+{
+    add(std::move(g));
+}
+
+std::string Collapser::toString()
+{
+    std::wstring ws = towstring(NameGenerator::toString());
+    std::wstring result;
+    result.reserve(ws.size());
+
+    int count = 0;
+    wchar_t prev = L'\0';
+    for (auto ch : ws) {
+        if (ch == prev) {
+            ++count;
+        } else {
+            count = 0;
+        }
+        // Default allow up to 2 repeats
+        int maxRepeat = 2;
+
+        // If you want to restrict certain letters to 1 repeat, etc.
+        // For demonstration, keep it simple and always use maxRepeat=2
+        // or adapt logic as needed.
+
+        if (count < maxRepeat) {
+            result.push_back(ch);
+        }
+        prev = ch;
+    }
+
+    return tostring(result);
+}
+
+}
---
+++ Utils/NameGenerator.h
@@ -0,0 +1,243 @@
+#ifndef NAMEGENERATOR_H
+#define NAMEGENERATOR_H
+
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <cwchar>
+#include <cwctype>
+#include <memory>
+#include <random>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace Daitengu::Utils {
+
+std::wstring towstring(const std::string& s);
+std::string tostring(const std::wstring& ws);
+
+class NameGenerator {
+public:
+    NameGenerator();
+    NameGenerator(const std::string& pattern, bool collapse_triples = true);
+    NameGenerator(std::vector<std::unique_ptr<NameGenerator>>&& generators_);
+
+    virtual ~NameGenerator() = default;
+
+    virtual std::size_t combinations();
+    virtual std::size_t min();
+    virtual std::size_t max();
+    virtual std::string toString();
+    void add(std::unique_ptr<NameGenerator>&& g);
+
+protected:
+    std::vector<std::unique_ptr<NameGenerator>> generators;
+
+private:
+    enum class wrappers { capitalizer, reverser };
+
+    enum class group_types { symbol, literal };
+
+    class Group {
+    public:
+        group_types type;
+        Group(group_types t);
+        virtual ~Group() = default;
+
+        std::unique_ptr<NameGenerator> produce();
+        void split();
+        void wrap(wrappers w);
+        void add(std::unique_ptr<NameGenerator>&& g);
+        virtual void add(char c);
+
+    protected:
+        std::stack<wrappers> wrapperStack;
+        std::vector<std::unique_ptr<NameGenerator>> set;
+    };
+
+    class GroupSymbol : public Group {
+    public:
+        GroupSymbol();
+        void add(char c) override; // override to expand symbol from SymbolMap
+    };
+
+    class GroupLiteral : public Group {
+    public:
+        GroupLiteral();
+    };
+
+    static const std::unordered_map<std::string,
+        const std::vector<std::string>>&
+    SymbolMap();
+};
+
+class Random : public NameGenerator {
+public:
+    Random() = default;
+    explicit Random(std::vector<std::unique_ptr<NameGenerator>>&& generators_);
+
+    std::size_t combinations() override;
+    std::size_t min() override;
+    std::size_t max() override;
+    std::string toString() override;
+};
+
+class Sequence : public NameGenerator {
+public:
+    Sequence() = default;
+    explicit Sequence(
+        std::vector<std::unique_ptr<NameGenerator>>&& generators_);
+};
+
+class Literal : public NameGenerator {
+public:
+    explicit Literal(const std::string& value_);
+
+    std::size_t combinations() override;
+    std::size_t min() override;
+    std::size_t max() override;
+    std::string toString() override;
+
+private:
+    std::string value;
+};
+
+class Reverser : public NameGenerator {
+public:
+    explicit Reverser(std::unique_ptr<NameGenerator>&& g);
+    std::string toString() override;
+};
+
+class Capitalizer : public NameGenerator {
+public:
+    explicit Capitalizer(std::unique_ptr<NameGenerator>&& g);
+    std::string toString() override;
+};
+
+class Collapser : public NameGenerator {
+public:
+    explicit Collapser(std::unique_ptr<NameGenerator>&& g);
+    std::string toString() override;
+};
+
+inline constexpr auto MIDDLE_EARTH
+    = "(bil|bal|ban|hil|ham|hal|hol|hob|wil|me|or|ol|od|gor|for|fos|tol|ar|fin|"
+      "ere|leo|vi|bi|bren|thor)"
+      "(|go|orbis|apol|adur|mos|ri|i|na|ole|n)"
+      "(|tur|axia|and|bo|gil|bin|bras|las|mac|grim|wise|l|lo|fo|co|ra|via|da|"
+      "ne|ta|y|wen|thiel|phin|dir|dor|tor|rod|on|rdo|dis)";
+
+inline constexpr auto JAPANESE_NAMES_CONSTRAINED
+    = "(aka|aki|bashi|gawa|kawa|furu|fuku|fuji|hana|hara|haru|hashi|hira|hon|"
+      "hoshi|ichi|iwa|kami|kawa|ki|kita|kuchi|kuro|marui|matsu|miya|mori|moto|"
+      "mura|nabe|naka|nishi|no|da|ta|o|oo|oka|saka|saki|sawa|shita|shima|i|"
+      "suzu|taka|take|to|toku|toyo|ue|wa|wara|wata|yama|yoshi|kei|ko|zawa|zen|"
+      "sen|ao|gin|kin|ken|shiro|zaki|yuki|asa)"
+      "(||||||||||bashi|gawa|kawa|furu|fuku|fuji|hana|hara|haru|hashi|hira|hon|"
+      "hoshi|chi|wa|ka|kami|kawa|ki|kita|kuchi|kuro|marui|matsu|miya|mori|moto|"
+      "mura|nabe|naka|nishi|no|da|ta|o|oo|oka|saka|saki|sawa|shita|shima|suzu|"
+      "taka|take|to|toku|toyo|ue|wa|wara|wata|yama|yoshi|kei|ko|zawa|zen|sen|"
+      "ao|gin|kin|ken|shiro|zaki|yuki|sa)";
+
+inline constexpr auto JAPANESE_NAMES_DIVERSE
+    = "(a|i|u|e|o|||||)(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|"
+      "se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|"
+      "ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|"
+      "ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)"
+      "(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|"
+      "chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|"
+      "mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|"
+      "ro|wa|wa|wa|wa|wo|wo)"
+      "(|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|"
+      "chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|"
+      "mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|"
+      "ro|ro|wa|wa|wa|wa|wo|wo)"
+      "|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|"
+      "chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|"
+      "mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|"
+      "ro|wa|wa|wa|wa|wo|wo)"
+      "(|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|"
+      "chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|"
+      "mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|"
+      "ro|ro|wa|wa|wa|wa|wo|wo)))(|||n)";
+
+inline constexpr auto CHINESE_NAMES
+    = "(zh|x|q|sh|h)(ao|ian|uo|ou|ia)"
+      "(|(l|w|c|p|b|m)(ao|ian|uo|ou|ia)(|n)|-(l|w|c|p|b|m)(ao|ian|uo|ou|ia)(|("
+      "d|j|q|l)(a|ai|iu|ao|i)))";
+
+inline constexpr auto GREEK_NAMES
+    = "<s<v|V>(tia)|s<v|V>(os)|B<v|V>c(ios)|B<v|V><c|C>v(ios|os)>";
+
+inline constexpr auto HAWAIIAN_NAMES_1
+    = "((h|k|l|m|n|p|w|')|)(a|e|i|o|u)((h|k|l|m|n|p|w|')|)(a|e|i|o|u)"
+      "(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)"
+      "(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)";
+
+inline constexpr auto HAWAIIAN_NAMES_2
+    = "((h|k|l|m|n|p|w|)(a|e|i|o|u|a'|e'|i'|o'|u'|ae|ai|ao|au|oi|ou|eu|ei)(k|l|"
+      "m|n|p|)|)"
+      "(h|k|l|m|n|p|w|)(a|e|i|o|u|a'|e'|i'|o'|u'|ae|ai|ao|au|oi|ou|eu|ei)(k|l|"
+      "m|n|p|)";
+
+inline constexpr auto OLD_LATIN_PLACE_NAMES = "sv(nia|lia|cia|sia)";
+
+inline constexpr auto DRAGONS_PERN
+    = "<<s|ss>|<VC|vC|B|BVs|Vs>><v|V|v|<v(l|n|r)|vc>>(th)";
+
+inline constexpr auto DRAGON_RIDERS = "c'<s|cvc>";
+
+inline constexpr auto POKEMON = "<i|s>v(mon|chu|zard|rtle)";
+
+inline constexpr auto FANTASY_VOWELS_R
+    = "(|(<B>|s|h|ty|ph|r))(i|ae|ya|ae|eu|ia|i|eo|ai|a)"
+      "(lo|la|sri|da|dai|the|sty|lae|due|li|lly|ri|na|ral|sur|rith)"
+      "(|(su|nu|sti|llo|ria|))(|(n|ra|p|m|lis|cal|deu|dil|suir|phos|ru|dru|rin|"
+      "raap|rgue))";
+
+inline constexpr auto FANTASY_S_A
+    = "(cham|chan|jisk|lis|frich|isk|lass|mind|sond|sund|ass|chad|lirt|und|mar|"
+      "lis|il|<BVC>)"
+      "(jask|ast|ista|adar|irra|im|ossa|assa|osia|ilsa|<vCv>)"
+      "(|(an|ya|la|sta|sda|sya|st|nya))";
+
+inline constexpr auto FANTASY_H_L
+    = "(ch|ch't|sh|cal|val|ell|har|shar|shal|rel|laen|ral|jh't|alr|ch|ch't|av)"
+      "(|(is|al|ow|ish|ul|el|ar|iel))"
+      "(aren|aeish|aith|even|adur|ulash|alith|atar|aia|erin|aera|ael|ira|iel|"
+      "ahur|ishul)";
+
+inline constexpr auto FANTASY_N_L
+    = "(ethr|qil|mal|er|eal|far|fil|fir|ing|ind|il|lam|quel|quar|quan|qar|pal|"
+      "mal|yar|um|ard|enn|ey)"
+      "(|(<vc>|on|us|un|ar|as|en|ir|ur|at|ol|al|an))"
+      "(uard|wen|arn|on|il|ie|on|iel|rion|rian|an|ista|rion|rian|cil|mol|yon)";
+
+inline constexpr auto FANTASY_K_N
+    = "(taith|kach|chak|kank|kjar|rak|kan|kaj|tach|rskal|kjol|jok|jor|jad|kot|"
+      "kon|knir|kror|kol|tul|rhaok|rhak|krol|jan|kag|ryr)"
+      "(<vc>|in|or|an|ar|och|un|mar|yk|ja|arn|ir|ros|ror)"
+      "(|(mund|ard|arn|karr|chim|kos|rir|arl|kni|var|an|in|ir|a|i|as))";
+
+inline constexpr auto FANTASY_J_G_Z
+    = "(aj|ch|etz|etzl|tz|kal|gahn|kab|aj|izl|ts|jaj|lan|kach|chaj|qaq|jol|ix|"
+      "az|biq|nam)"
+      "(|(<vc>|aw|al|yes|il|ay|en|tom||oj|im|ol|aj|an|as))"
+      "(aj|am|al|aqa|ende|elja|ich|ak|ix|in|ak|al|il|ek|ij|os|al|im)";
+
+inline constexpr auto FANTASY_K_J_Y
+    = "(yi|shu|a|be|na|chi|cha|cho|ksa|yi|shu)(th|dd|jj|sh|rr|mk|n|rk|y|jj|th)"
+      "(us|ash|eni|akra|nai|ral|ect|are|el|urru|aja|al|uz|ict|arja|ichi|ural|"
+      "iru|aki|esh)";
+
+inline constexpr auto FANTASY_S_E
+    = "(syth|sith|srr|sen|yth|ssen|then|fen|ssth|kel|syn|est|bess|inth|nen|tin|"
+      "cor|sv|iss|ith|sen|slar|ssil|sthen|svis|s|ss|s|ss)"
+      "(|(tys|eus|yn|of|es|en|ath|elth|al|ell|ka|ith|yrrl|is|isl|yr|ast|iy))"
+      "(us|yn|en|ens|ra|rg|le|en|ith|ast|zon|in|yn|ys)";
+
+}
+#endif // NAMEGENERATOR_H

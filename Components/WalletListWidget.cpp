ab30182ddd4b983806adb0660cce6221aa14d1d8
to1dev <admin@to1.dev>
2025-02-26 01:16:05 +0100

    update WalletListWidget component

--- Components/WalletListWidget.cpp
+++ Components/WalletListWidget.cpp
@@ -2,6 +2,28 @@
 
 namespace Daitengu::Components {
 
+void BadgeItemDelegate::paint(QPainter* painter,
+    const QStyleOptionViewItem& option, const QModelIndex& index) const
+{
+    QStyledItemDelegate::paint(painter, option, index);
+
+    QVariant data
+        = index.data(static_cast<int>(WalletListWidget::ItemData::selected));
+    if (data.isValid() && data.toBool()) {
+        QIcon badgeIcon(":/Badges/check.svg");
+        QSize badgeSize(32, 32);
+        // QPoint badgePosition(option.rect.topRight().x() - badgeSize.width(),
+        // option.rect.topRight().y() - 6);
+        QPoint badgePosition(
+            option.rect.topLeft().x(), option.rect.topRight().y() - 6);
+        QRect badgeRect(badgePosition, badgeSize);
+
+        painter->save();
+        badgeIcon.paint(painter, badgeRect);
+        painter->restore();
+    }
+}
+
 WalletListWidget::WalletListWidget(QWidget* parent)
     : QListWidget(parent)
 {
@@ -14,6 +36,64 @@ WalletListWidget::WalletListWidget(QWidget* parent)
     horizontalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
     setDragEnabled(false);
     setContextMenuPolicy(Qt::CustomContextMenu);
+
+    setItemDelegate(new BadgeItemDelegate(this));
+}
+
+bool WalletListWidget::focusChanged()
+{
+    return currentItem() ? !currentItem()->isSelected() : false;
+}
+
+void WalletListWidget::add(const Wallet& wallet, int index)
+{
+    QListWidgetItem* item = new QListWidgetItem;
+    QString name = QString::fromStdString(wallet.name);
+    item->setText(name);
+    item->setIcon(
+        QIcon(QString::fromUtf8(WalletListIcons[wallet.type].second.data())));
+    item->setToolTip(name);
+
+    item->setData(static_cast<int>(ItemData::selected), false);
+    item->setData(static_cast<int>(ItemData::id), wallet.id);
+    item->setData(static_cast<int>(ItemData::index), index);
+    item->setData(static_cast<int>(ItemData::type), wallet.type);
+    item->setData(
+        static_cast<int>(ItemData::hash), QString::fromStdString(wallet.hash));
+    item->setData(static_cast<int>(ItemData::name), name);
+    item->setData(static_cast<int>(ItemData::mnemonic),
+        QString::fromStdString(wallet.mnemonic));
+
+    addItem(item);
+
+    if (selectedId_ == wallet.id) {
+        item->setSelected(true);
+        setCurrentRow(row(item));
+        scrollToItem(item);
+        // setFocus();
+    }
+}
+
+void WalletListWidget::update(const Wallet& wallet)
+{
+    QListWidgetItem* item = currentItem();
+    QString name = QString::fromStdString(wallet.name);
+    item->setText(name);
+    item->setToolTip(name);
+    item->setData(static_cast<int>(ItemData::name), name);
+}
+
+void WalletListWidget::purge()
+{
+    QList<QListWidgetItem*> items = selectedItems();
+    if (!items.isEmpty()) {
+        qDeleteAll(items);
+    }
+}
+
+void WalletListWidget::setSelectedId(int newSelectedId)
+{
+    selectedId_ = newSelectedId;
 }
 
 }
--- Components/WalletListWidget.h
+++ Components/WalletListWidget.h
@@ -1,21 +1,72 @@
 #ifndef WALLETLISTWIDGET_H
 #define WALLETLISTWIDGET_H
 
+#include <array>
+
 #include <QListWidget>
 #include <QPainter>
 #include <QScrollBar>
 #include <QStyledItemDelegate>
 
+#include "Databases/Database.h"
+
+using namespace Daitengu::Databases;
+
 namespace Daitengu::Components {
+
+inline constexpr char WALLET_ICON_DEFAULT[] = ":/List/wallet.svg";
+inline constexpr char WALLET_ICON_KEY[] = ":/List/key.svg";
+inline constexpr char WALLET_ICON_MNEMONIC[] = ":/List/mnemonic.svg";
+inline constexpr char WALLET_ICON_TRADE[] = ":/List/trade.svg";
+inline constexpr char WALLET_ICON_SEARCH[] = ":/List/search.svg";
+inline constexpr char WALLET_ICON_GOLD[] = ":/List/gold.svg";
+inline constexpr char WALLET_ICON_COINS[] = ":/List/coins.svg";
 
 inline constexpr char WALLET_OBJECT_NAME[] = "listWidgetWallet";
 
 inline constexpr int WALLET_ICON_SIZE = 64;
 inline constexpr int WALLET_SPACING_SIZE = 12;
 
+inline constexpr std::array<std::pair<int, std::string_view>, 5> WalletListIcons
+    = { {
+        { 0, WALLET_ICON_DEFAULT },
+        { 1, WALLET_ICON_MNEMONIC },
+        { 2, WALLET_ICON_KEY },
+        { 3, WALLET_ICON_KEY },
+        { 4, WALLET_ICON_SEARCH },
+    } };
+
+class BadgeItemDelegate : public QStyledItemDelegate {
+public:
+    using QStyledItemDelegate::QStyledItemDelegate;
+    void paint(QPainter* painter, const QStyleOptionViewItem& option,
+        const QModelIndex& index) const override;
+};
+
 class WalletListWidget : public QListWidget {
 public:
+    enum class ItemData {
+        selected = Qt::UserRole + 100,
+        type,
+        id,
+        index,
+        hash,
+        name,
+        mnemonic,
+        derivationPath,
+    };
+
     explicit WalletListWidget(QWidget* parent = nullptr);
+    bool focusChanged();
+
+    void add(const Wallet& wallet, int index = 0);
+    void update(const Wallet& wallet);
+    void purge();
+
+    void setSelectedId(int newSelectedId);
+
+private:
+    int selectedId_ { -1 };
 };
 
 }

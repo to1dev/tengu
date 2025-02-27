57437b93bf8d1f0ea3f912b13a2ade2ac11d6595
to1dev <admin@to1.dev>
2025-02-27 03:53:32 +0100

    update UpdateWalletForm

--- Forms/UpdateWalletForm.cpp
+++ Forms/UpdateWalletForm.cpp
@@ -1,10 +1,22 @@
 #include "UpdateWalletForm.h"
 #include "ui_UpdateWalletForm.h"
 
-UpdateWalletForm::UpdateWalletForm(QWidget* parent)
+UpdateWalletForm::UpdateWalletForm(
+    QWidget* parent, const std::shared_ptr<const GlobalManager>& globalManager)
     : QDialog(parent)
     , ui(new Ui::UpdateWalletForm)
 {
+    ui->setupUi(this);
+
+    setWindowTitle(DEFAULT_TITLE);
+
+    frameless_ = std::make_unique<Frameless>(this);
+    frameless_->setMainFrame(ui->frameMain);
+    frameless_->setContentFrame(ui->frameContent);
+    frameless_->init();
+
+    windowManager_->setWindow(this);
+    windowManager_->reset(0.6);
 }
 
 UpdateWalletForm::~UpdateWalletForm()
--- Forms/UpdateWalletForm.h
+++ Forms/UpdateWalletForm.h
@@ -2,6 +2,14 @@
 
 #include <QDialog>
 
+#include "Managers/GlobalManager.h"
+#include "Managers/WindowManager.h"
+
+#include "UI/Frameless.h"
+
+using namespace Daitengu::Core;
+using namespace Daitengu::UI;
+
 namespace Ui {
 class UpdateWalletForm;
 }
@@ -9,10 +17,19 @@ class UpdateWalletForm;
 class UpdateWalletForm : public QDialog {
     Q_OBJECT
 
+    const QString DEFAULT_TITLE = QObject::tr("Edit Wallet");
+
 public:
-    explicit UpdateWalletForm(QWidget* parent = nullptr);
+    explicit UpdateWalletForm(QWidget* parent = nullptr,
+        const std::shared_ptr<const GlobalManager>& globalManager = nullptr);
     ~UpdateWalletForm();
 
 private:
     Ui::UpdateWalletForm* ui;
+
+    std::shared_ptr<const GlobalManager> globalManager_;
+    std::unique_ptr<WindowManager> windowManager_ {
+        std::make_unique<WindowManager>()
+    };
+    std::unique_ptr<Frameless> frameless_;
 };

0df68a838e7b055ae5a0dd85fa0607fe7b78e34f
to1dev <admin@to1.dev>
2025-02-28 02:15:35 +0100

    update ComboBoxEx

--- src/Components/ComboBoxEx.cpp
+++ src/Components/ComboBoxEx.cpp
@@ -1,13 +1,56 @@
 #include "ComboBoxEx.h"
 
+PopupListWidget::PopupListWidget(QWidget* parent)
+    : QListWidget(parent)
+{
+    setObjectName("popupList");
+}
+
+void PopupListWidget::keyPressEvent(QKeyEvent* event)
+{
+    if (event->key() == Qt::Key_Escape) {
+        hide();
+        event->accept();
+        return;
+    }
+
+    if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter) {
+        if (currentItem()) {
+            Q_EMIT enterKeyPressed(currentItem());
+            event->accept();
+            return;
+        }
+    }
+
+    QListWidget::keyPressEvent(event);
+}
+
 ComboBoxEx::ComboBoxEx(QWidget* parent)
     : QLineEdit(parent)
-    , listWidget_(new QListWidget)
+    , listWidget_(new PopupListWidget(nullptr))
+    , currentIndex_(-1)
 {
+    setReadOnly(true);
+
     listWidget_->setWindowFlags(Qt::Popup);
+    listWidget_->setFocusPolicy(Qt::NoFocus);
+    listWidget_->setAttribute(Qt::WA_DeleteOnClose, false);
+
     connect(listWidget_, &QListWidget::itemClicked, this,
         &ComboBoxEx::itemSelected);
+    connect(listWidget_, &PopupListWidget::enterKeyPressed, this,
+        &ComboBoxEx::handleEnterKeyPressed);
     connect(this, &QLineEdit::textEdited, this, &ComboBoxEx::filterList);
+
+    setupEventFilter();
+}
+
+ComboBoxEx::~ComboBoxEx()
+{
+    if (listWidget_) {
+        listWidget_->removeEventFilter(this);
+        listWidget_->deleteLater();
+    }
 }
 
 void ComboBoxEx::setItems(const QStringList& items)
@@ -23,16 +66,93 @@ void ComboBoxEx::addItem(const QString& item)
     filterList(text());
 }
 
+void ComboBoxEx::setCurrentIndex(int index)
+{
+    if (index < -1 || index >= items_.size()) {
+        return;
+    }
+
+    if (index == -1) {
+        currentIndex_ = -1;
+        setText("");
+    } else {
+        currentIndex_ = index;
+        setText(items_[index]);
+    }
+
+    Q_EMIT currentIndexChanged(currentIndex_);
+}
+
 void ComboBoxEx::mousePressEvent(QMouseEvent* event)
 {
     QLineEdit::mousePressEvent(event);
     showPopup();
 }
 
+void ComboBoxEx::keyPressEvent(QKeyEvent* event)
+{
+    switch (event->key()) {
+    case Qt::Key_Down:
+    case Qt::Key_Up:
+        showPopup();
+        if (listWidget_->count() > 0) {
+            listWidget_->setCurrentRow(
+                listWidget_->currentRow() < 0 ? 0 : listWidget_->currentRow());
+            listWidget_->setFocus();
+        }
+        event->accept();
+        break;
+
+    case Qt::Key_Return:
+    case Qt::Key_Enter:
+        if (listWidget_->isVisible() && listWidget_->currentItem()) {
+            handleEnterKeyPressed(listWidget_->currentItem());
+            event->accept();
+        } else {
+            QLineEdit::keyPressEvent(event);
+        }
+        break;
+
+    default:
+        QLineEdit::keyPressEvent(event);
+        break;
+    }
+}
+
+bool ComboBoxEx::eventFilter(QObject* watched, QEvent* event)
+{
+    if (event->type() == QEvent::MouseButtonPress) {
+        if (listWidget_->isVisible()) {
+            QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+
+            QPoint globalPos;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+            globalPos = mouseEvent->globalPosition().toPoint();
+#else
+            globalPos = mouseEvent->globalPos();
+#endif
+
+            if (!listWidget_->geometry().contains(globalPos)
+                && !QRect(mapToGlobal(QPoint(0, 0)), size())
+                    .contains(globalPos)) {
+                hidePopup();
+                return true;
+            }
+        }
+    }
+
+    return QObject::eventFilter(watched, event);
+}
+
 void ComboBoxEx::itemSelected(QListWidgetItem* item)
 {
+    if (!item)
+        return;
+
+    currentIndex_ = listWidget_->row(item);
     setText(item->text());
-    listWidget_->hide();
+    Q_EMIT currentIndexChanged(currentIndex_);
+    hidePopup();
 }
 
 void ComboBoxEx::filterList(const QString& text)
@@ -47,8 +167,13 @@ void ComboBoxEx::filterList(const QString& text)
     if (listWidget_->count() > 0) {
         showPopup();
     } else {
-        listWidget_->hide();
+        hidePopup();
     }
+}
+
+void ComboBoxEx::handleEnterKeyPressed(QListWidgetItem* item)
+{
+    itemSelected(item);
 }
 
 void ComboBoxEx::showPopup()
@@ -56,8 +181,33 @@ void ComboBoxEx::showPopup()
     if (listWidget_->count() == 0)
         return;
 
-    QPoint pos = mapToGlobal(QPoint(0, height()));
+    QPoint pos = mapToGlobal(QPoint(0, height() + 3));
     listWidget_->move(pos);
     listWidget_->resize(width(), 150);
+
+    if (listWidget_->currentRow() < 0 && listWidget_->count() > 0) {
+        listWidget_->setCurrentRow(0);
+    }
+
+    if (currentIndex_ >= 0 && currentIndex_ < listWidget_->count()) {
+        for (int i = 0; i < listWidget_->count(); ++i) {
+            if (listWidget_->item(i)->text() == items_[currentIndex_]) {
+                listWidget_->setCurrentRow(i);
+                break;
+            }
+        }
+    }
+
     listWidget_->show();
+}
+
+void ComboBoxEx::hidePopup()
+{
+    listWidget_->hide();
+    setFocus();
+}
+
+void ComboBoxEx::setupEventFilter()
+{
+    listWidget_->installEventFilter(this);
 }
--- src/Components/ComboBoxEx.h
+++ src/Components/ComboBoxEx.h
@@ -1,29 +1,66 @@
 #pragma once
 
+#include <QFocusEvent>
+#include <QKeyEvent>
 #include <QLineEdit>
 #include <QListWidget>
 #include <QMouseEvent>
 #include <QVBoxLayout>
+
+class PopupListWidget : public QListWidget {
+    Q_OBJECT
+
+public:
+    explicit PopupListWidget(QWidget* parent = nullptr);
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
+
+Q_SIGNALS:
+    void enterKeyPressed(QListWidgetItem* currentItem);
+};
 
 class ComboBoxEx : public QLineEdit {
     Q_OBJECT
 
 public:
     explicit ComboBoxEx(QWidget* parent = nullptr);
+    ~ComboBoxEx() override;
 
     void setItems(const QStringList& items);
     void addItem(const QString& item);
 
+    [[nodiscard]] int currentIndex() const
+    {
+        return currentIndex_;
+    }
+
+    [[nodiscard]] QString currentText() const
+    {
+        return text();
+    }
+
+    void setCurrentIndex(int index);
+
+Q_SIGNALS:
+    void currentIndexChanged(int index);
+
 protected:
     void mousePressEvent(QMouseEvent* event) override;
+    void keyPressEvent(QKeyEvent* event) override;
+    bool eventFilter(QObject* watched, QEvent* event) override;
 
 private Q_SLOTS:
     void itemSelected(QListWidgetItem* item);
     void filterList(const QString& text);
+    void handleEnterKeyPressed(QListWidgetItem* item);
 
 private:
     void showPopup();
+    void hidePopup();
+    void setupEventFilter();
 
-    QListWidget* listWidget_;
+    PopupListWidget* listWidget_;
     QStringList items_;
+    int currentIndex_;
 };

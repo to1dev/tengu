0f87f85d48cc3c546c79c56416fa0e23f7984032
to1dev <admin@to1.dev>
2025-03-03 04:47:47 +0100

    update WalletDock

--- src/Forms/WalletDock.cpp
+++ src/Forms/WalletDock.cpp
@@ -1,8 +1,26 @@
 #include "WalletDock.h"
+#include "ui_WalletDock.h"
 
-WalletDock::WalletDock(QWidget* parent)
+WalletDock::WalletDock(
+    QWidget* parent, const std::shared_ptr<const GlobalManager>& globalManager)
     : QWidget(parent)
+    , ui(new Ui::WalletDock)
+    , globalManager_(globalManager)
 {
-    setWindowFlags(Qt::FramelessWindowHint | Qt::Tool);
-    resize(400, 800);
+    ui->setupUi(this);
+
+    setWindowTitle(DEFAULT_TITLE);
+
+    frameless_ = std::make_unique<Frameless>(this);
+    frameless_->setMainFrame(ui->frameMain);
+    frameless_->setContentFrame(ui->frameContent);
+    frameless_->init(Frameless::Mode::PANEL);
+
+    globalManager_->windowManager()->addWindow(this);
+    globalManager_->windowManager()->center(this);
+}
+
+WalletDock::~WalletDock()
+{
+    delete ui;
 }
--- src/Forms/WalletDock.h
+++ src/Forms/WalletDock.h
@@ -2,9 +2,31 @@
 
 #include <QWidget>
 
+#include "Managers/GlobalManager.h"
+#include "Managers/WindowManager.h"
+
+#include "UI/Frameless.h"
+
+using namespace Daitengu::Core;
+using namespace Daitengu::UI;
+
+namespace Ui {
+class WalletDock;
+}
+
 class WalletDock : public QWidget {
     Q_OBJECT
 
+    const QString DEFAULT_TITLE = QObject::tr("My Wallet");
+
 public:
-    explicit WalletDock(QWidget* parent = nullptr);
+    explicit WalletDock(QWidget* parent = nullptr,
+        const std::shared_ptr<const GlobalManager>& globalManager = nullptr);
+    ~WalletDock();
+
+private:
+    Ui::WalletDock* ui;
+
+    std::shared_ptr<const GlobalManager> globalManager_;
+    std::unique_ptr<Frameless> frameless_;
 };

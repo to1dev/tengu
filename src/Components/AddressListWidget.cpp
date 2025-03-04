b5820a1473c92b43c394edc1e144dc872e7e07a3
to1dev <admin@to1.dev>
2025-03-04 05:44:27 +0100

    add AddressListWidget component

---
+++ src/Components/AddressListWidget.cpp
@@ -0,0 +1,236 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "AddressListWidget.h"
+
+namespace Daitengu::Components {
+
+BoldFirstLineDelegate::BoldFirstLineDelegate(QObject* parent)
+    : QStyledItemDelegate(parent)
+{
+}
+
+void BoldFirstLineDelegate::paint(QPainter* painter,
+    const QStyleOptionViewItem& option, const QModelIndex& index) const
+{
+    QStyleOptionViewItem options = option;
+    initStyleOption(&options, index);
+
+    options.text = QString();
+    options.widget->style()->drawControl(
+        QStyle::CE_ItemViewItem, &options, painter, options.widget);
+
+    const QString name
+        = index.data(static_cast<int>(AddressListWidget::ItemData::name))
+              .toString();
+    const int i
+        = index.data(static_cast<int>(AddressListWidget::ItemData::index))
+              .toInt()
+        + 1;
+    const QString address = hideAddress(
+        index.data(static_cast<int>(AddressListWidget::ItemData::address))
+            .toString());
+
+    const QString displayName
+        = name.isEmpty() ? QString(STR_ADDRESS_NAME).arg(i) : name;
+
+    painter->save();
+
+    QFont boldFont = options.font;
+    boldFont.setBold(true);
+
+    QFont regularFont = options.font;
+    regularFont.setUnderline(true);
+
+    const QFontMetrics boldMetrics(boldFont);
+    const QFontMetrics regularMetrics(regularFont);
+
+    const QRect textRect = options.widget->style()->subElementRect(
+        QStyle::SE_ItemViewItemText, &options);
+    const int totalTextHeight
+        = boldMetrics.height() + regularMetrics.height() + 8;
+
+    const int xOffset = textRect.left() + 8;
+    int yOffset = textRect.top() + (textRect.height() - totalTextHeight) / 2;
+    const int availableWidth = textRect.width() - 16;
+
+    const QString elidedName
+        = boldMetrics.elidedText(displayName, Qt::ElideRight, availableWidth);
+    const QString elidedAddress
+        = regularMetrics.elidedText(address, Qt::ElideRight, availableWidth);
+
+    painter->setFont(boldFont);
+    painter->drawText(xOffset, yOffset, availableWidth,
+        boldMetrics.height() + 8, Qt::AlignLeft | Qt::AlignTop, elidedName);
+    yOffset += boldMetrics.height() + 8;
+
+    painter->setFont(regularFont);
+    painter->drawText(xOffset, yOffset, availableWidth, regularMetrics.height(),
+        Qt::AlignLeft | Qt::AlignTop, elidedAddress);
+
+    painter->restore();
+}
+
+bool BoldFirstLineDelegate::eventFilter(QObject* object, QEvent* event)
+{
+    if (event->type() == QEvent::MouseMove) {
+        if (auto* mouseEvent = dynamic_cast<QMouseEvent*>(event)) {
+            auto* listView = qobject_cast<QListView*>(parent());
+            if (listView) {
+                const QModelIndex index = listView->indexAt(mouseEvent->pos());
+                const QRect rect = listView->visualRect(index);
+                const QFontMetrics metrics(listView->font());
+                const QRect addressRect(rect.left() + 8,
+                    rect.top() + metrics.height() + 16, rect.width() - 16,
+                    metrics.height());
+
+                hoverOverAddress = addressRect.contains(mouseEvent->pos());
+                listView->viewport()->setCursor(hoverOverAddress
+                        ? Qt::PointingHandCursor
+                        : Qt::ArrowCursor);
+                return true;
+            }
+        }
+    } else if (event->type() == QEvent::MouseButtonPress && hoverOverAddress) {
+        if (auto* mouseEvent = dynamic_cast<QMouseEvent*>(event)) {
+            if (mouseEvent->button() == Qt::LeftButton) {
+                auto* listView = qobject_cast<QListView*>(parent());
+                if (listView) {
+                    const QModelIndex index
+                        = listView->indexAt(mouseEvent->pos());
+                    const QRect rect = listView->visualRect(index);
+                    const QFontMetrics metrics(listView->font());
+                    const QRect addressRect(rect.left() + 8,
+                        rect.top() + metrics.height() + 16, rect.width() - 16,
+                        metrics.height());
+
+                    if (addressRect.contains(mouseEvent->pos())) {
+                        QApplication::clipboard()->setText(index
+                                .data(static_cast<int>(
+                                    AddressListWidget::ItemData::address))
+                                .toString());
+                    }
+                }
+            }
+        }
+    }
+
+    return QObject::eventFilter(object, event);
+}
+
+AddressListWidget::AddressListWidget(QWidget* parent)
+    : QListWidget(parent)
+{
+    setObjectName(ADDRESS_OBJECT_NAME);
+    setIconSize(QSize(ADDRESS_ICON_SIZE, ADDRESS_ICON_SIZE));
+    setSpacing(ADDRESS_SPACING_SIZE);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    verticalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
+    horizontalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
+    setContextMenuPolicy(Qt::CustomContextMenu);
+
+    auto* delegate = new BoldFirstLineDelegate(this);
+    setItemDelegate(delegate);
+    viewport()->installEventFilter(delegate);
+
+    connect(this, &QListWidget::itemDoubleClicked, this,
+        &AddressListWidget::copyItemTextToClipboard);
+}
+
+void AddressListWidget::add(const Address& address, int index)
+{
+    auto* item = new QListWidgetItem();
+
+    item->setText("Dummy Text First\nSecond");
+
+    item->setData(static_cast<int>(ItemData::selected), false);
+    item->setData(static_cast<int>(ItemData::id), address.id);
+    item->setData(static_cast<int>(ItemData::type), address.type);
+    item->setData(static_cast<int>(ItemData::walletId), address.walletId);
+    item->setData(static_cast<int>(ItemData::index), index);
+    item->setData(
+        static_cast<int>(ItemData::hash), QString::fromStdString(address.hash));
+    item->setData(
+        static_cast<int>(ItemData::name), QString::fromStdString(address.name));
+    item->setData(static_cast<int>(ItemData::address),
+        QString::fromStdString(address.address));
+    item->setData(static_cast<int>(ItemData::derivationPath),
+        QString::fromStdString(address.derivationPath));
+
+    addItem(item);
+
+    if (selectedId_ == address.id) {
+        item->setSelected(true);
+        setCurrentRow(row(item));
+        scrollToItem(item);
+    }
+}
+
+void AddressListWidget::load(const std::vector<Address>& addresses)
+{
+    setUpdatesEnabled(false);
+
+    clear();
+    int index = 0;
+    for (const auto& address : addresses) {
+        add(address, index++);
+    }
+
+    setUpdatesEnabled(true);
+}
+
+void AddressListWidget::update(const Address& address)
+{
+    QListWidgetItem* item = currentItem();
+    if (!item)
+        return;
+
+    item->setData(
+        static_cast<int>(ItemData::name), QString::fromStdString(address.name));
+    item->setData(static_cast<int>(ItemData::address),
+        QString::fromStdString(address.address));
+}
+
+void AddressListWidget::purge()
+{
+    while (!selectedItems().isEmpty()) {
+        delete takeItem(row(selectedItems().first()));
+    }
+}
+
+bool AddressListWidget::focusChanged()
+{
+    return currentItem() ? !currentItem()->isSelected() : false;
+}
+
+void AddressListWidget::setSelectedId(int newSelectedId)
+{
+    selectedId_ = newSelectedId;
+}
+
+void AddressListWidget::copyItemTextToClipboard(QListWidgetItem* item)
+{
+    if (item) {
+        const QString address
+            = item->data(static_cast<int>(AddressListWidget::ItemData::address))
+                  .toString();
+        QApplication::clipboard()->setText(address);
+    }
+}
+
+}
---
+++ src/Components/AddressListWidget.h
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QApplication>
+#include <QClipboard>
+#include <QListWidget>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QScrollBar>
+#include <QStyledItemDelegate>
+
+#include "Consts.h"
+
+#include "Databases/Database.h"
+
+#include "Utils/Helpers.hpp"
+
+using namespace Daitengu::Core;
+using namespace Daitengu::Databases;
+using namespace Daitengu::Utils;
+
+namespace Daitengu::Components {
+
+inline constexpr char ADDRESS_OBJECT_NAME[] = "listWidgetAddress";
+inline constexpr int ADDRESS_ICON_SIZE = 64;
+inline constexpr int ADDRESS_SPACING_SIZE = 3;
+
+class BoldFirstLineDelegate : public QStyledItemDelegate {
+public:
+    explicit BoldFirstLineDelegate(QObject* parent = nullptr);
+    ~BoldFirstLineDelegate() override = default;
+
+    void paint(QPainter* painter, const QStyleOptionViewItem& option,
+        const QModelIndex& index) const override;
+
+protected:
+    bool eventFilter(QObject* object, QEvent* event) override;
+
+private:
+    mutable bool hoverOverAddress { false };
+};
+
+class AddressListWidget : public QListWidget {
+public:
+    enum class ItemData {
+        selected = Qt::UserRole + 200,
+        id,
+        type,
+        walletId,
+        index,
+        hash,
+        name,
+        address,
+        derivationPath,
+    };
+
+    explicit AddressListWidget(QWidget* parent = nullptr);
+    ~AddressListWidget() override = default;
+
+    void add(const Address& address, int index = 0);
+    void load(const std::vector<Address>& addresses);
+    void update(const Address& address);
+    void purge();
+
+    bool focusChanged();
+
+    void setSelectedId(int newSelectedId);
+
+private Q_SLOTS:
+    void copyItemTextToClipboard(QListWidgetItem* item);
+
+private:
+    int selectedId_ { -1 };
+};
+
+}

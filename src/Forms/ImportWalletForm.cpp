8eea7b27e68efbdd50d05164caa5065084a57c81
to1dev <admin@to1.dev>
2025-03-10 22:03:05 +0100

    add ImportWalletForm

---
+++ src/Forms/ImportWalletForm.cpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ImportWalletForm.h"
+#include "ui_ImportWalletForm.h"
+
+ImportWalletForm::ImportWalletForm(
+    QWidget* parent, const std::shared_ptr<const GlobalManager>& globalManager)
+    : QDialog(parent)
+    , ui(new Ui::ImportWalletForm)
+    , globalManager_(globalManager)
+{
+    ui->setupUi(this);
+
+    setWindowTitle(DEFAULT_TITLE);
+
+    frameless_ = std::make_unique<Frameless>(this);
+    frameless_->setMainFrame(ui->frameMain);
+    frameless_->setContentFrame(ui->frameContent);
+    frameless_->init(Frameless::Mode::DIALOG);
+
+    globalManager_->windowManager()->reset(this, 0.7);
+    connect(frameless_.get(), &Frameless::onMax, this,
+        [this]() { globalManager_->windowManager()->reset(this, 0.7); });
+}
+
+ImportWalletForm::~ImportWalletForm()
+{
+    delete ui;
+}
---
+++ src/Forms/ImportWalletForm.h
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QDialog>
+
+#include "Consts.h"
+
+#include "Managers/GlobalManager.h"
+#include "Managers/WindowManager.h"
+
+#include "UI/Frameless.h"
+
+using namespace Daitengu::Core;
+using namespace Daitengu::UI;
+
+namespace Ui {
+class ImportWalletForm;
+}
+
+class ImportWalletForm : public QDialog {
+    Q_OBJECT
+
+    const QString DEFAULT_TITLE = QObject::tr("Import Wallet");
+
+public:
+    explicit ImportWalletForm(QWidget* parent = nullptr,
+        const std::shared_ptr<const GlobalManager>& globalManager = nullptr);
+    ~ImportWalletForm();
+
+private:
+    Ui::ImportWalletForm* ui;
+
+    std::shared_ptr<const GlobalManager> globalManager_;
+    std::unique_ptr<Frameless> frameless_;
+};

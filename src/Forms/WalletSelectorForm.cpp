743af205455f878e2a0a8e60bf2f780b2900fc8c
to1dev <admin@to1.dev>
2025-03-19 18:40:01 +0100

    add WalletSelectorForm

---
+++ src/Forms/WalletSelectorForm.cpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "WalletSelectorForm.h"
+#include "ui_WalletSelectorForm.h"
+
+WalletSelectorForm::WalletSelectorForm(
+    QWidget* parent, const std::shared_ptr<const GlobalManager>& globalManager)
+    : QDialog(parent)
+    , ui(new Ui::WalletSelectorForm)
+    , globalManager_(globalManager)
+    , addressRecord_(std::make_shared<Address>())
+{
+    ui->setupUi(this);
+
+    setWindowTitle(DEFAULT_TITLE);
+
+    frameless_ = std::make_unique<Frameless>(this);
+    frameless_->setMainFrame(ui->frameMain);
+    frameless_->setContentFrame(ui->frameContent);
+    frameless_->init(Frameless::Mode::DIALOG);
+}
+
+WalletSelectorForm::~WalletSelectorForm()
+{
+    delete ui;
+}
+
+std::shared_ptr<Address> WalletSelectorForm::addressRecord() const
+{
+    return addressRecord_;
+}
+
+void WalletSelectorForm::ok()
+{
+}

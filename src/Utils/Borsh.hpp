9570ae2e8e41d12b9909f50b89d2a250e991cc78
to1dev <admin@to1.dev>
2025-03-23 04:17:58 +0100

    add Borsh.hpp

---
+++ src/Utils/Borsh.hpp
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+/*
+ * Copyright (C) 2025 to1dev <https://arc20.me/to1dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <array>
+#include <concepts>
+#include <cstdint>
+#include <cstring>
+#include <map>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+namespace borsh {
+
+class BorshError : public std::runtime_error {
+public:
+    explicit BorshError(const std::string& what)
+        : std::runtime_error(what)
+    {
+    }
+};
+
+}

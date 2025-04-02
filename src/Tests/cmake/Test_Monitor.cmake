8a0c3bcbfc209e1878e5a0fbab3b6ee44f87c19d
to1dev <admin@to1.dev>
2025-04-02 08:59:25 +0200

    add monitor test

---
+++ src/Tests/cmake/Test_Monitor.cmake
@@ -0,0 +1,73 @@
+project(test_monitor LANGUAGES CXX)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+set(BUILD_SHARED_LIBS OFF)
+
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTOUIC ON)
+set(CMAKE_AUTORCC ON)
+
+find_package(Qt5 REQUIRED COMPONENTS
+    Core
+    Network
+    WebSockets
+    Concurrent
+)
+
+file(GLOB ALL_SOURCES
+    "${CMAKE_SOURCE_DIR}/src/Wallets/Core/*.cpp"
+    "${CMAKE_SOURCE_DIR}/src/Wallets/Utils/*.cpp"
+    "${CMAKE_SOURCE_DIR}/src/Wallets/Monitor/*.cpp"
+    "${CMAKE_SOURCE_DIR}/src/Wallets/Monitor/Bitcoin/*.cpp"
+    "${CMAKE_SOURCE_DIR}/src/Utils/Encryption.cpp"
+)
+
+set(TEST_SOURCES
+    ${CMAKE_SOURCE_DIR}/src/tests/Test_Monitor.cpp
+)
+
+add_executable(${PROJECT_NAME} ${ALL_SOURCES} ${TEST_SOURCES})
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+    ${CMAKE_SOURCE_DIR}/src
+    ${CMAKE_SOURCE_DIR}/src/3rd/inc
+    ${CMAKE_SOURCE_DIR}/src/3rd/inc/izanagi
+)
+
+target_link_directories(${PROJECT_NAME} PRIVATE
+    ${CMAKE_SOURCE_DIR}/src/3rd/lib
+)
+
+target_compile_definitions(${PROJECT_NAME} PRIVATE    SECP256K1_STATIC
+    QT_STATIC
+    QT_NO_KEYWORDS
+)
+
+target_compile_options(${PROJECT_NAME} PRIVATE
+    -Wno-unused-parameter
+    -Wno-template-id-cdtor
+    -Wno-tautological-compare
+    -Wno-unused-local-typedefs
+    -Wno-volatile
+    -Wno-misleading-indentation
+    -Wno-unused-but-set-parameter
+)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE
+    xxhash
+    sodium
+    izanagi
+    secp256k1
+    spdlog
+    Qt5::Core
+    Qt5::Network
+    Qt5::WebSockets
+    Qt5::Concurrent
+    QCoro5Core
+    QCoro5Network
+    QCoro5WebSockets
+)

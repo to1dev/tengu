19879f861c72ed93812c837d33fdaccb2d9c03b2
to1dev <admin@to1.dev>
2025-04-29 09:52:30 +0200

    cleanup 3rd again

--- src/3rd/inc/qcoro/qcoro.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Daniel Vrátil <dvratil@kde.org>
-//
-// SPDX-License-Identifier: MIT
-
-#pragma once
-
-#include "qcorocore.h"
-#include "qcoronetwork.h"
--- src/3rd/inc/qcoro/qcorowebsockets.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Daniel Vrátil <dvratil@kde.org>
-//
-// SPDX-License-Identifier: MIT
-
-#pragma once
-
-#include "qcorowebsocket.h"
-#include "qcorowebsocketserver.h"
